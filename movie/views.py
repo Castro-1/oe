786bc81e8f39711f81ead9046a9390684b8f231c
Castro-1 <jecastrog@eafit.edu.co>
2023-02-09 10:37:15 -0500

    cambios en la funcion home.

--- movie/views.py
+++ movie/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html',{'name': 'El man es german'})
+    return render(request, 'home.html',{'name': 'Juan Castro'})
